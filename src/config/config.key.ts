3b783d738c5d288745fda5a667d48719b142e212
chrisShikayri <cdb.jimenez.@yavirac.edu.ec>
2024-11-16 04:37:14 -0500

    clase_8 realisado

---
+++ src/config/config.key.ts
@@ -0,0 +1,7 @@
+export enum configuracio {
+    PORT= 'PORT',
+    HOST='HOST',
+    USERNAME='USERNAME',
+    PASSWORD='PASSWORD',
+    DATABASE='DATABASE'
+}
---
+++ src/database/database.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { databaseProvider } from './database.providers';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+    providers:[...databaseProvider,ConfigService],
+    exports:[...databaseProvider]
+})
+export class DatabaseModule {
+
+}
